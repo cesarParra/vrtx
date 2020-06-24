1578aa63ad8d6b88cd9ad8305baee7e840e7bbbf
Cesar Parra <cesar.parra@gmail.com>
2020-06-23 21:02:23 -0400

    Configuration file support.

--- src/parser/builder/__tests__/builder.test.ts
+++ src/parser/builder/__tests__/builder.test.ts
@@ -1,10 +1,12 @@
 import FileReader from "../../../utils/file-reader";
 import Translator from "../../translator/translator";
 import Builder from "../builder";
-jest.mock("../../utils/file-reader");
-jest.mock("../../utils/file-writer");
-jest.mock("../translator");
+import fs from "fs";
+jest.mock("../../../utils/file-reader");
+jest.mock("../../../utils/file-writer");
+jest.mock("../../translator/translator");
 jest.mock("prettier");
+jest.mock("fs");
 
 describe("builder", () => {
   beforeEach(() => {
@@ -12,14 +14,26 @@ describe("builder", () => {
     (<jest.Mock>(<unknown>Translator)).mockClear();
   });
 
+  function mockConfigFileRead() {
+    (fs.readFileSync as jest.Mock).mockReturnValue(`{
+      "compilerOptions": {
+        "targetApiVersion": 48.0,
+        "rootDir": "src",
+        "targetDir": "lib"
+      }
+    }`);
+  }
+
   test("File reader is called", () => {
+    mockConfigFileRead();
     ((FileReader.read as unknown) as jest.Mock).mockReturnValue([]);
-    Builder.execute("");
+    Builder.execute();
     expect(FileReader.read).toHaveBeenCalled();
     expect(Translator.translate).toHaveBeenCalledTimes(0);
   });
 
   test("Translator is called ", () => {
+    mockConfigFileRead();
     ((FileReader.read as unknown) as jest.Mock).mockReturnValue([
       {
         name: "Test",
@@ -27,12 +41,8 @@ describe("builder", () => {
         extension: "vrtx",
       },
     ]);
-    Builder.execute("");
+    Builder.execute();
     expect(FileReader.read).toHaveBeenCalled();
     expect(Translator.translate).toHaveBeenCalled();
   });
-
-  function mockRimRaf(outputDir: string, callback: () => void) {
-    callback();
-  }
 });
--- src/parser/model/apex-metadata.ts
+++ src/parser/model/apex-metadata.ts
@@ -1,6 +1,12 @@
 import Configuration from "../builder/configuration";
 
-const apiVersion = Configuration.get().compilerOptions.targetApiVersion;
+let apiVersion;
+try {
+  apiVersion = Configuration.get().compilerOptions.targetApiVersion;
+} catch (e) {
+  apiVersion = 48.0;
+}
+
 export const APEX_METADATA = `<?xml version="1.0" encoding="UTF-8"?>
 <ApexClass xmlns="http://soap.sforce.com/2006/04/metadata">
     <apiVersion>${apiVersion}</apiVersion>
